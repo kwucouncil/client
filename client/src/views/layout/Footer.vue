c8d823972f0d6d048e65c792300a813524c6d08c
gusrns48 <gusrns48@naver.com>
2025-02-05 17:08:37 +0900

    [ENH] pc 메인디자인

--- client/src/views/layout/Footer.vue
+++ client/src/views/layout/Footer.vue
@@ -1,10 +1,21 @@
 <template>
 	<footer>
 		<div class="footer-wrap">
+			<div class="logo-wrap">
+				<img src="@/assets/imgs/common/logo_w.svg" alt="광운대학교">
+			</div>
+			<p class="intro">광운대학교 제52대 총학생회 비상대책위원회</p>
+			<p class="info">
+				주소 : 서울시 노원구 광운로 20 광운대학교 복지관 301호 총학생회실<br>
+				TEL : 000-0000-0000<br>
+				E-Mail : kwustudent2025@gmail.com<br>
+				사업자등록번호 : 000-00-00000
+			</p>
+			<p class="copyright">COPYRIGHT <b>2025 Kwangwoon University Student Council.</b> ALL RIGHTS RESERVED.</p>
 		</div>
 	</footer>
 </template>
 
 <style scoped lang="scss">
-@import "@/scss/layout/footer";
+	@import "@/scss/layout/footer";
 </style>
