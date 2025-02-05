5aa30f922a060f01b98e7ac5bb0cbfc68dcb76f4
gusrns48 <gusrns48@naver.com>
2025-02-05 16:04:37 +0900

    [ENH] 공지사항 수정

--- client/src/views/main/Main.vue
+++ client/src/views/main/Main.vue
@@ -12,6 +12,95 @@
           <h2>새로배움터 신청 조회</h2>
           <router-link to="/">신청 조회하기</router-link>
         </div>
+        <div class="card-wrap">
+          <div class="notice-wrap">
+            <div class="title-wrap">
+              <h3>공지사항</h3>
+              <router-link to="/">공지사항 더보기</router-link>
+            </div>
+            <div class="table-wrap">
+              <table>
+                <thead>
+                  <tr>
+                    <th>제목</th>
+                    <th>작성일</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td class="title">2025 총학생회 홈페이지 공지사항</td>
+                    <td class="date">2025.01.01</td>
+                  </tr>
+                  <tr>
+                    <td class="title">2025 총학생회 홈페이지 공지사항</td>
+                    <td class="date">2025.01.01</td>
+                  </tr>
+                  <tr>
+                    <td class="title">2025 총학생회 홈페이지 공지사항</td>
+                    <td class="date">2025.01.01</td>
+                  </tr>
+                  <tr>
+                    <td class="title">2025 총학생회 홈페이지 공지사항</td>
+                    <td class="date">2025.01.01</td>
+                  </tr>
+                  <tr>
+                    <td class="title">2025 총학생회 홈페이지 공지사항</td>
+                    <td class="date">2025.01.01</td>
+                  </tr>
+                  <tr>
+                    <td class="title">2025 총학생회 홈페이지 공지사항</td>
+                    <td class="date">2025.01.01</td>
+                  </tr>
+                  <tr>
+                    <td class="title">2025 총학생회 홈페이지 공지사항</td>
+                    <td class="date">2025.01.01</td>
+                  </tr>
+                  <tr>
+                    <td class="title">2025 총학생회 홈페이지 공지사항</td>
+                    <td class="date">2025.01.01</td>
+                  </tr>
+                  <tr>
+                    <td class="title">2025 총학생회 홈페이지 공지사항</td>
+                    <td class="date">2025.01.01</td>
+                  </tr>
+                  <tr>
+                    <td class="title">2025 총학생회 홈페이지 공지사항</td>
+                    <td class="date">2025.01.01</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <!-- <div class="events-wrap">
+            <div class="title-wrap">
+              <h3>월별 주요 행사</h3>
+            </div>
+            <div class="table-wrap">
+              <table>
+                <thead>
+                  <tr>
+                    <th>제목</th>
+                    <th>작성일</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td class="title">2025 총학생회 홈페이지 공지사항</td>
+                    <td class="date">2025.01.01</td>
+                  </tr>
+                  <tr>
+                    <td class="title">2025 총학생회 홈페이지 공지사항</td>
+                    <td class="date">2025.01.01</td>
+                  </tr>
+                  <tr>
+                    <td class="title">2025 총학생회 홈페이지 공지사항</td>
+                    <td class="date">2025.01.01</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div> -->
+        </div>
       </section>
     </div>
   </div>
