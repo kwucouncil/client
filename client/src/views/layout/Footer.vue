c7ebe1dd92f86cda76d7965efe240cecabaf6b85
gusrns48 <gusrns48@naver.com>
2025-02-08 16:47:56 +0900

    [ENH] footer edit

--- client/src/views/layout/Footer.vue
+++ client/src/views/layout/Footer.vue
@@ -7,9 +7,9 @@
 			<p class="intro">광운대학교 제52대 총학생회 비상대책위원회</p>
 			<p class="info">
 				주소 : 서울시 노원구 광운로 20 광운대학교 복지관 301호 총학생회실<br>
-				TEL : 000-0000-0000<br>
+				TEL : 02-940-5381<br>
 				E-Mail : kwustudent2025@gmail.com<br>
-				사업자등록번호 : 000-00-00000
+				<!-- 사업자등록번호 : 000-00-00000 -->
 			</p>
 			<p class="copyright">COPYRIGHT <b>2025 Kwangwoon University Student Council.</b> ALL RIGHTS RESERVED.</p>
 		</div>
