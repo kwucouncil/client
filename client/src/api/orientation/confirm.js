3a6ee517f3a1fe90fcf980718b3d74052a4aeabb
unknown <jehohi04@naver.com>
2025-02-10 15:47:29 +0900

    [ENH] confirm.js 수정

--- client/src/api/orientation/confirm.js
+++ client/src/api/orientation/confirm.js
@@ -1,11 +1,11 @@
-import Send from '@/api/Send.js'
+import Send from '@/api/Send.js';
 
 export default {
   confirm(data) {
     return Send({
       method: 'post',
-      url: '/api',
+      url: 'https://backend-pi-nine-35.vercel.app/api',
       data: data,
-    })
+    });
   },
-}
+};
